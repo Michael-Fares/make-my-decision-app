8affa7a149cf325c0c59d55df386b27ee2dd6e72
Michael-Fares <mjfares@gmail.com>
2021-10-24 12:33:03 -0500

    managed to validate jwt on backend finally

--- src/components/AddDecision.js
+++ src/components/AddDecision.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios'
 import { 
   Button,
   TextField,
@@ -6,20 +7,52 @@ import {
   Typography,
   Stack
  } from "@mui/material";
- import { Link } from 'react-router-dom'
+ import { Link, useHistory } from 'react-router-dom'
+
+ import cookie from 'cookie'
+
+const token = localStorage.getItem('token')
+
+axios.defaults.headers.common['authorization'] = `Bearer ${token}`;
+
+ 
 
 const AddDecision = (props) => {
+
+  const [decision, setDecision] = useState("")
+
+  const handleChange = (e) => {
+    e.preventDefault(e)
+    setDecision(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault(e)
+
+
+    axios.post('http://localhost:4001/decisions', 
+    {
+      decision_text: decision
+    })
+      .then(res => console.log(res))
+      .catch(err => console.log('there was an error', err))
+
+    console.log('the cookie token from front end', token)
+    setDecision("")
+  }
+
   return (
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
           <Box mb={2}>
             <Typography variant="h6">What Decision Would You Help Making?</Typography>
           </Box>
           
         <Stack spacing={3}>
-        <TextField required="true" placeholder="Type Anything!" label="Type Anything!"/>
+        <TextField name="decision" value={decision} required="true" placeholder="Type Anything!" label="Type Anything!" onChange={(e)=>{handleChange(e)}}/>
             <Button fullWidth = {true} 
             variant="contained"
             className="form-margin"
+            type="submit"
             >
             Add
           </Button>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios'
 import { 
   Button,
   TextField,
@@ -6,21 +7,75 @@ import {
   Typography,
   Stack
  } from "@mui/material";
- import { Link } from 'react-router-dom'
+ import { Link, useHistory } from 'react-router-dom'
 
 const Login = (props) => {
+
+  let history = useHistory();
+
+  const [user, setUser] = useState({
+    email: "",
+    password: ""
+  })
+
+  const handleChange = (e) => {
+    e.preventDefault()
+    const loggedInUser = { ... user }
+    loggedInUser[e.target.name] = e.target.value
+    setUser(loggedInUser)
+  }
+  
+  const handleSubmit = (e) => {
+    console.log(e)
+
+    e.preventDefault()
+
+
+    // use axios to post to server from 311_wk4_day2
+    // posts just a new user fname and lname to users table
+    // got it to work in firefox turning off CORS
+    // but not in Chrome
+    axios.post('http://localhost:4001/users/login', {
+      email: user.email,
+      password: user.password,
+    })
+    .then(function (response) {
+      console.log('response', response);
+      // set current user info in local storage
+      // this worked
+      localStorage.setItem("token", response.data.accessToken);
+      localStorage.setItem("id", response.data.id);
+      localStorage.setItem("email", response.data.email)
+      localStorage.setItem("name", response.data.first_name)
+      localStorage.setItem("loggedIn", true)
+     delete response.accessToken;
+    }).then(() => history.push("/decisions"))
+    .catch(function (error) {
+      console.log('THERE WAS AN ERROR', error);
+    });
+
+    // reset the form
+    setUser( 
+      {
+        email: "",
+        password: ""
+      }
+     )
+  }
+
   return (
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
           <Box mb={2}>
             <Typography variant="h6">Welcome! Please Login</Typography>
           </Box>
           
         <Stack spacing={1}>
-        <TextField required="true" placeholder="Email" label="Email"/>
-        <TextField required="true" placeholder="Password" type="password" label="Password"/ >
+        <TextField name="email" value={user.email} required="true" placeholder="Email" label="Email" onChange={(e)=>{handleChange(e)}} />
+        <TextField name="password" value={user.password} required="true" placeholder="Password" type="password" label="Password" onChange={(e)=>{handleChange(e)}} / >
             <Button fullWidth = {true} 
             variant="contained"
             className="form-margin"
+            type="submit"
             >
             Login
           </Button>
