60c2cc42cbff67dec7425d3e80aa87206802c3eb
Michael-Fares <mjfares@gmail.com>
2021-11-12 18:43:07 -0600

    added background image to landing page that look nicer

--- src/components/About.js
+++ src/components/About.js
@@ -11,8 +11,9 @@ import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 
 const About = () => {
   return (
-    <>
-    <Stack direction="row" spacing={5} alignItems="space-evenly" justifyContent="center">
+    <div className="img-container">
+        
+    <Stack direction="row" spacing={1} justifyContent="space-around" alignItems="flex-end" flexWrap="wrap">
         <div className="home-container">
         <Typography color="secondary" variant="h4" mt={2}>Welcome to <i>Make My Decision</i> !</Typography>
        
@@ -20,7 +21,7 @@ const About = () => {
         <Typography color="primary" variant="h6">How it works:</Typography>
         <ol>
           <Stack spacing={1}>
-          <Typography color="secondary"><li>Enter the decision you need help making.</li>
+          <Typography variant="h6"><li>Enter the decision you need help making.</li>
           <li>Tell us about the criteria important to you in this decision. You can enter as many as you need to!</li>
           <li>Tell us about the options you have to choose from for this decision. You can enter as many as you want!</li>
           <li>Tell us how you feel about each option based on each criteria.</li>
@@ -30,15 +31,16 @@ const About = () => {
         {checkAuth() ? <Box mt={2}><Link to="/decisions"><Button endIcon={<ArrowRightIcon/>} fullWidth={true} variant="contained">Go to my Saved Decisions</Button></Link></Box>  
         : <Box mt={2}><Link to="/signup"><Button fullWidth={true} variant="contained">Get Started!</Button></Link></Box>}
         </div>
-        <div className="img-container">
-        </div>
+        <Typography variant="h5" color="primary">
+            <i>- Emotions wane. Reasons remain.</i>
+        </Typography>
     </Stack>
     <Container>
-      <Stack spacing={1} direction="row" justifyContent="flex-end" alignItems="center">
-       <Typography color="primary" mt={4}><i>- Emotions wane. Reasons remain.</i></Typography>
+      <Stack spacing={1} direction="row" justifyContent="flex-start" alignItems="center">
+   
       </Stack>
     </Container>
-  </>
+  </div>
   )
 }
 
